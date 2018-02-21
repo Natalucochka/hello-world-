5c91e316bc00751d52d1103a08e8c5f7d1ed373a
Nataly <natalucochka@gmail.com>
2018-02-21 15:11:29 +0200

    js1

---
+++ js.js
@@ -0,0 +1,10 @@
+function CAPTCHA () { 
+   
+   // alert("Hello world");
+    var check1 =  document.getElementById('takevalue').value;
+    var check2 =  document.getElementById('take').value;
+     if (check2 == check1) 
+    alert ("Все вірно!");
+    else 
+    alert ("Не правильно! первірте ще раз");
+     }
